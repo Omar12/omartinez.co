e5023a479eec8ddc201123acd1db35b0529f968c
Omar Martinez <this.is.omartinez@gmail.com>
2020-07-15 22:32:41 -0700

    fixinf text on home

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -11,13 +11,15 @@ const IndexPage = props => {
     <Layout>
       <SEO title="Home" />
       {posts.edges.map(({ node }) => (
-        <Link to={node.fields.slug} key={node.fields.slug} className="link">
-          <div className="post-list">
-            <h1>{node.frontmatter.title}</h1>
-            <span>{node.frontmatter.date}</span>
-            <p>{node.excerpt}</p>
-          </div>
-        </Link>
+        <div className="home-list">
+          <Link to={node.fields.slug} key={node.fields.slug} className="link">
+            <div className="post-list">
+              <h1>{node.frontmatter.title}</h1>
+              <span>{node.frontmatter.date}</span>
+              <p>{node.excerpt}</p>
+            </div>
+          </Link>
+        </div>
       ))}
     </Layout>
   )
