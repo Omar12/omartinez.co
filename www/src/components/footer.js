f403960bf158955004b30f3bb5129bda05ea441c
Omar Martinez <this.is.omartinez@gmail.com>
2021-01-03 17:34:22 -0800

    Footer width fix

--- www/src/components/footer.js
+++ www/src/components/footer.js
@@ -4,19 +4,11 @@ import About from "../pages/about"
 
 const Footer = () => (
   <footer className="bg-anakiwa-200 text-steel-gray-500">
-    <div className="px-10 py-6 w-4/6">
+    <div className="px-10 py-6 sm:w-4/6">
       <About />
       <div className="pt-3 text-sm">{new Date().getFullYear()}</div>
     </div>
   </footer>
 )
-
-Footer.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Footer.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Footer
