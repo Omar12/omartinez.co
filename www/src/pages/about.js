8a86666a86709543eaf454ef4a293dda6c21312e
Omar Martinez <this.is.omartinez@gmail.com>
2021-01-24 16:38:20 -0800

    changing to h3 in about

--- www/src/pages/about.js
+++ www/src/pages/about.js
@@ -3,7 +3,7 @@ import AboutLinks from "../components/aboutLinks"
 
 const AboutPage = () => (
   <div className="text-perfume-500 font-light text-sm">
-    <h4 className="text-xl pb-2">Who Am I?</h4>
+    <h3 className="text-xl pb-2">Who Am I?</h3>
     <p>
       My name is Omar Martinez. I’m a Lead Frontend Software Developer based out
       of Seattle. I've been a developer professionally for 16 years. I’ve been
